466e01c04e2c6d8c93bcbb396606641c4dfe8c78
equinochrome <122951356+equinochrome@users.noreply.github.com>
2025-03-20 14:38:03 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+
+
+
+
+void initialize() {
+	
+}
+
+
+void disabled() {}
+
+
+void competition_initialize() {}
+
+
+void autonomous() {}
+
+
+void opcontrol() {
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+
+	while (true) {
+ 
+	}
+}
